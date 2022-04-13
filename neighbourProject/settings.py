8caa8ee1b51fa0df3b466d5c3aac16a4b84f0a3e
silvianjoki <10silvianjoki@gmail.com>
2022-04-13 20:44:33 +0300

    update migrations

--- neighbourProject/settings.py
+++ neighbourProject/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'django_bootstrap_icons',
+    "bootstrap5",
+    'neighbourApp.apps.NeighbourappConfig',
+
 ]
 
 MIDDLEWARE = [
