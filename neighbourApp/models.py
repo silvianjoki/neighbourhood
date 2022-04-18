2ca7a6c779297f0fa357f47ca5f25c5a28b4d4b8
silvianjoki <10silvianjoki@gmail.com>
2022-04-18 18:30:03 +0300

    add models

--- neighbourApp/models.py
+++ neighbourApp/models.py
@@ -1,3 +1,89 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class neighbourhood(models.Model):
+    name= models.CharField(max_length=255)
+    location= models.CharField(max_length=255)
+    occupants= models.IntegerField()
+    admin=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.location
+    
+    def create_neighbourhood(self):
+        return self.save()
+    
+    def delete_neighbourhood(self):
+        self.delete()
+    
+    @classmethod
+    def find_neighbourhood(cls,id):
+        return cls.objects.filter(id=id)
+    
+    @classmethod
+    def update_neighbourhood(cls,id, name):
+        return cls.objects.filter(id=id).update(name=name)
+    
+    @classmethod
+    def update_occupants(cls, id, name):
+        return cls.objects.filter(id=id).update(name=name)
+    
+
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    name= models.CharField(max_length=255)
+    profile_pic = models.ImageField(upload_to = 'images/', null=True)
+    
+    
+    def __str__(self):
+        return self.user.username
+    
+    def save_admin(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete() 
+# name.
+# id.
+# neighborhood id foreign key
+# email address.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    neighbourhood = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_id = models.IntegerField()
+    
+    def __str__(self):
+        return self.user.username
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete() 
+        
+
+
+class Business(models.Model):
+    name= models.CharField(max_length=255)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood_id= models.ForeignKey(User, on_delete=models.CASCADE)
+    email=models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.user.username
+    
+    def create_business(self):
+        self.save()
+        
+    @classmethod
+    def find_business(cls,id):
+        return cls.objects.filter(id=id)
+    
+    @classmethod
+    def update_business(cls,id, name):
+        return cls.objects.filter(id=id).update(name=name)
