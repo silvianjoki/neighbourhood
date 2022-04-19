97f8ba922576d0982f5536df066da8cc81ff7fb5
silvianjoki <10silvianjoki@gmail.com>
2022-04-19 14:05:57 +0300

    add superuser and admin

--- neighbourApp/admin.py
+++ neighbourApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, Business, Post, Admin, Neighbourhood
+
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Admin)
+admin.site.register(Neighbourhood)
